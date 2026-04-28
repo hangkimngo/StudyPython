cf6ccfd70d32ee35efa915f6d408096f5e402c57
Kim Hang Ngo <82223275+hangkimngo@users.noreply.github.com>
2026-04-28 16:51:32 +0300

    Create filter_forbidden.py

---
+++ filter_forbidden.py
@@ -0,0 +1,8 @@
+def filter_forbidden(string:str, forbidden:str):
+    return "".join([character for character in string if character not in forbidden])
+
+if __name__ == "__main__":
+
+    sentence = "Once! upon, a time: there was a python!??!?!"
+    filtered = filter_forbidden(sentence, "!?:,.")
+    print(filtered)
